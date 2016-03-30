06c9a3347572a36abcb4c7abb6586174fc89a66d
LABELNET <1406046087@qq.com>
2016-03-30 21:41:28 +0800

    【ADD】 springmvc 配置

---
+++ src/main/java/cn/hpu/yuan/ssm/exception/CustomExceptionResover.java
@@ -0,0 +1,8 @@
+package cn.hpu.yuan.ssm.exception;
+
+/**
+ * Created by yuan on 16-3-30.
+ * 配置异常统一处理拦截类
+ */
+public class CustomExceptionResover {
+}
---
+++ src/main/java/cn/hpu/yuan/ssm/filter/HandlerIntercepter.java
@@ -0,0 +1,8 @@
+package cn.hpu.yuan.ssm.filter;
+
+/**
+ * Created by yuan on 16-3-30.
+ * 过滤拦截器示例
+ */
+public class HandlerIntercepter {
+}
