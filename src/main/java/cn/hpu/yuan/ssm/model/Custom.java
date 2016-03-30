babfda8f32d34e0822c689cfaf3fdcf98635e85c
LABELNET <1406046087@qq.com>
2016-03-30 16:49:16 +0800

    【EDIT】 创建拓展的pojo对象和其他的服务的pojo类

--- src/main/java/cn/hpu/yuan/ssm/model/Custom.java
+++ src/main/java/cn/hpu/yuan/ssm/model/Custom.java
@@ -2,6 +2,7 @@ package cn.hpu.yuan.ssm.model;
 
 /**
  * Created by yuan on 16-3-30.
+ * 通用的实体类
  */
 public class Custom {
 }
---
+++ src/main/java/cn/hpu/yuan/ssm/model/custom/UserCustom.java
@@ -0,0 +1,8 @@
+package cn.hpu.yuan.ssm.model.custom;
+
+/**
+ * Created by yuan on 16-3-30.
+ * 用户其他实体类
+ */
+public class UserCustom {
+}
