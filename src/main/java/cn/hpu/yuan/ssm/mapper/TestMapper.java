220ec21235203c33f6b43e76796da5e0045b1f48
LABELNET <1406046087@qq.com>
2016-04-01 16:31:34 +0800

    【EDIT】删除mapper测试和model测试 两个类

--- src/main/java/cn/hpu/yuan/ssm/mapper/TestMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.hpu.yuan.ssm.mapper;
-
-/**
- * Created by yuan on 16-3-30.
- */
-public interface TestMapper {
-}
--- src/main/java/cn/hpu/yuan/ssm/model/Custom.java
+++
@@ -1,8 +0,0 @@
-package cn.hpu.yuan.ssm.model;
-
-/**
- * Created by yuan on 16-3-30.
- * 通用的实体类
- */
-public class Custom {
-}
