697d45e241a1ad1799a7b5b6f781ed3155fbd3c8
LABELNET <1406046087@qq.com>
2016-04-03 21:45:13 +0800

    【EDIT】新增表 - 评论表 和 点赞表 实体对象和拓展类

---
+++ src/main/java/cn/hpu/yuan/ssm/model/base/Comment.java
@@ -0,0 +1,77 @@
+package cn.hpu.yuan.ssm.model.base;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by yuan on 16-4-3.
+ */
+@Entity
+public class Comment {
+    private int id;
+    private String content;
+    private int status;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "content")
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Basic
+    @Column(name = "status")
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Comment comment = (Comment) o;
+
+        if (id != comment.id) return false;
+        if (status != comment.status) return false;
+        if (content != null ? !content.equals(comment.content) : comment.content != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + status;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
---
+++ src/main/java/cn/hpu/yuan/ssm/model/vo/CommentVo.java
@@ -0,0 +1,10 @@
+package cn.hpu.yuan.ssm.model.vo;
+
+import cn.hpu.yuan.ssm.model.base.Comment;
+
+/**
+ * Created by yuan on 16-4-3.
+ * 留言拓展类
+ */
+public class CommentVo extends Comment{
+}
