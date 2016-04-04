8a0437eef97f451ed8de62518741f4831cd4a1f6
LABELNET <1406046087@qq.com>
2016-04-04 23:09:47 +0800

    【EDIT】后台管理－点赞管理－添加新闻来源别名

--- src/main/java/cn/hpu/yuan/ssm/mapper/manager/SourceManagerMapper.java
+++ src/main/java/cn/hpu/yuan/ssm/mapper/manager/SourceManagerMapper.java
@@ -5,4 +5,11 @@ package cn.hpu.yuan.ssm.mapper.manager;
  * 后台管理 - 新闻来源管理 - mapper
  */
 public interface SourceManagerMapper {
+
+    //分页查询　－　新闻来源管理　－　id 倒序输出
+
+    //修改　－　新闻来源内容
+    //删除　- 新闻来源内容
+    //新增　- 新闻来源
+
 }
