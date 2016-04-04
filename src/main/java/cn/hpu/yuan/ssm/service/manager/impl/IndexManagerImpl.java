3c98316b8a277efcff66510818ea20ea9788b100
LABELNET <1406046087@qq.com>
2016-04-04 23:47:01 +0800

    【EDIT】业务层－后台管理－用户管理－实现类添加

--- src/main/java/cn/hpu/yuan/ssm/service/manager/impl/IndexManagerImpl.java
+++ src/main/java/cn/hpu/yuan/ssm/service/manager/impl/IndexManagerImpl.java
@@ -6,5 +6,5 @@ import cn.hpu.yuan.ssm.service.manager.IndexManager;
  * Created by yuan on 16-4-4.
  * 业务层　－　后台管理 － 主页业务层 - 接口实现
  */
-public class IndexManagerImlp  implements IndexManager{
+public class IndexManagerImpl implements IndexManager{
 }
---
+++ src/main/java/cn/hpu/yuan/ssm/service/manager/impl/UserManagerImpl.java
@@ -0,0 +1,10 @@
+package cn.hpu.yuan.ssm.service.manager.impl;
+
+import cn.hpu.yuan.ssm.service.manager.UserManager;
+
+/**
+ * Created by yuan on 16-4-4.
+ * 业务层　－　后台管理 － 用户管理 - 接口实现
+ */
+public class UserManagerImpl implements UserManager{
+}
