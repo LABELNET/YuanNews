f718e17f5e3d2617f33ae0d2dbaf15e7b572fa71
LABELNET <1406046087@qq.com>
2016-04-04 23:42:51 +0800

    【EDIT】业务层－后台管理－评论管理和分类管理接口实现类添加

---
+++ src/main/java/cn/hpu/yuan/ssm/service/manager/impl/CommentManagerImpl.java
@@ -0,0 +1,10 @@
+package cn.hpu.yuan.ssm.service.manager.impl;
+
+import cn.hpu.yuan.ssm.service.manager.CommentManager;
+
+/**
+ * Created by yuan on 16-4-4.
+ * 业务层　－　后台管理 － 评论管理 - 接口实现
+ */
+public class CommentManagerImpl implements CommentManager{
+}
