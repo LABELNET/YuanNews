2c7ad97b15c474c956d2ee6b4164dacc113455cb
LABELNET <1406046087@qq.com>
2016-04-04 23:45:37 +0800

    【EDIT】业务层－后台管理－来源管理和兴趣管理实现类添加

---
+++ src/main/java/cn/hpu/yuan/ssm/service/manager/impl/SourceManagerImpl.java
@@ -0,0 +1,10 @@
+package cn.hpu.yuan.ssm.service.manager.impl;
+
+import cn.hpu.yuan.ssm.service.manager.SourceManager;
+
+/**
+ * Created by yuan on 16-4-4.
+ * 业务层　－　后台管理 － 来源管理 - 接口实现
+ */
+public class SourceManagerImpl implements SourceManager{
+}
