21af13c7349d4ee15a8e163e786cf0d4b8f10abc
LABELNET <1406046087@qq.com>
2016-04-04 23:23:30 +0800

    【EDIT】后台管理－来源单元测试实现

--- src/main/test/mapper/manager/SourceManagerTest.java
+++ src/main/test/mapper/manager/SourceManagerTest.java
@@ -1,11 +1,15 @@
 package mapper.manager;
 
 import cn.hpu.yuan.ssm.mapper.manager.SourceManagerMapper;
+import cn.hpu.yuan.ssm.model.vo.SourceVo;
 import common.ManagerBaseTest;
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * Created by yuan on 16-4-4.
+ * 后台管理－来源管理-单元测试
  */
 public class SourceManagerTest extends ManagerBaseTest{
 
@@ -18,8 +22,9 @@ public class SourceManagerTest extends ManagerBaseTest{
     }
 
     @Test
-    private void findSources(){
-
+    public void findSources() throws Exception {
+        List<SourceVo> sources = sourceManagerMapper.findSources(0, 4);
+        System.out.printf("来源测试　：　"+sources);
     }
 
 }
