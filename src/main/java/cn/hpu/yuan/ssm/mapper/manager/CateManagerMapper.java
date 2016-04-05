c30ca7a0630dcfd800d38f48abe7988eeccdeb2b
LABELNET <1406046087@qq.com>
2016-04-05 10:19:57 +0800

    【ADD】数据层－后台管理－分类管理－抛出异常

--- src/main/java/cn/hpu/yuan/ssm/mapper/manager/CateManagerMapper.java
+++ src/main/java/cn/hpu/yuan/ssm/mapper/manager/CateManagerMapper.java
@@ -12,18 +12,18 @@ import java.util.List;
 public interface CateManagerMapper {
 
     //根据分类id，查询分类信息
-    CateVo findCateById(Integer id);
+    CateVo findCateById(Integer id) throws Exception;
 
     //获取全部分类信息
-    List<CateVo> findCates();
+    List<CateVo> findCates() throws Exception;
 
     //修改分类信息
-    Integer updateCate(@Param("id") Integer id,@Param("content") String content);
+    Integer updateCate(@Param("id") Integer id,@Param("content") String content) throws Exception;
 
     //删除分类信息 : 管理的时候需要先判断是否存在正在使用的分类信息（在新闻中判断该分类下的新闻）
-    Integer deleteCate(Integer id);
+    Integer deleteCate(Integer id) throws Exception;
 
     //添加分类信息
-    Integer insertCate(String content);
+    Integer insertCate(String content) throws Exception;
 
 }
