cf86a22f3d1d1d7359f6649471284ae93d0cfdce
LABELNET <1406046087@qq.com>
2016-04-05 16:31:23 +0800

    【EDIT】拦截器－实现登陆判断－在进入主页的时候，判断其是否已经登陆了

--- src/main/java/cn/hpu/yuan/ssm/filter/LoginHandlerIntercepter.java
+++ src/main/java/cn/hpu/yuan/ssm/filter/LoginHandlerIntercepter.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpSession;
 /**
  * Created by yuan on 16-3-30.
  * 过滤拦截器示例
+ *
+ * 作用　：　用户在访问后台主页的时候，进行登陆判断
+ * 　　　　　如果登陆过，直接进行主页，否则转向登陆页，让其登陆
  */
 public class LoginHandlerIntercepter implements HandlerInterceptor {
 
@@ -17,28 +20,26 @@ public class LoginHandlerIntercepter implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request,
                                 HttpServletResponse response, Object arg2, Exception arg3)
             throws Exception {
-
     }
 
     public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
                            Object arg2, ModelAndView arg3) throws Exception {
         // TODO Auto-generated method stub
-
     }
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse arg1,
                              Object arg2) throws Exception {
         String requestURI = request.getRequestURI();
-        if(requestURI.indexOf("editClientIfo.action")>0){
-            //说明处在编辑的页面
+        if(requestURI.indexOf("managerIndex.action")>0){
+            //访问后台主页
             HttpSession session = request.getSession();
-            String username = (String) session.getAttribute("username");
+            String username = (String) session.getAttribute("uid");
             if(username!=null){
                 //登陆成功的用户
                 return true;
             }else{
                 //没有登陆，转向登陆界面
-                request.getRequestDispatcher("/login.jsp").forward(request,arg1);
+                request.getRequestDispatcher("/manager/LoginManger.jsp").forward(request,arg1);
                 return false;
             }
         }else{
