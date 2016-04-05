f0bd29f94a484607399ac6a1fa31d985c6e793e5
LABELNET <1406046087@qq.com>
2016-04-05 13:14:10 +0800

    【EDIT】工具类　－　日志工具类　－　使用可变参数优化最终版

--- src/main/java/cn/hpu/yuan/ssm/common/LoggerUtil.java
+++ src/main/java/cn/hpu/yuan/ssm/common/LoggerUtil.java
@@ -7,32 +7,15 @@ package cn.hpu.yuan.ssm.common;
 public class LoggerUtil {
 
 
-    public static void print(String msg,String msg1,String msg2,String msg3,String msg4){
+    public static void print(String... params){
+        String[] strings = params.clone();
         System.out.println("＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋");
         System.out.println("＋＋");
-        System.out.println("＋＋　　msg0 :　 "+msg);
-        System.out.println("＋＋　　msg1 :　 "+msg1);
-        System.out.println("＋＋　　msg2 : 　"+msg2);
-        System.out.println("＋＋　　msg3 : 　"+msg3);
-        System.out.println("＋＋　　msg4 :　 "+msg4);
+        for(int i=0;i<strings.length;i++){
+            System.out.println("＋＋  arg"+i+"   :   "+strings[i]);
+        }
         System.out.println("＋＋");
         System.out.println("＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋＋");
-    }
-
-    public static void print(String msg,String msg1,String msg2,String msg3) {
-        print(msg, msg1, msg2, msg3,null);
-    }
-
-    public static void print(String msg,String msg1,String msg2) {
-        print(msg, msg1, msg2,null);
-    }
-
-    public static void print(String msg,String msg1) {
-        print(msg, msg1,null);
-    }
-
-    public static void print(String msg) {
-        print(msg,null);
     }
 
 
