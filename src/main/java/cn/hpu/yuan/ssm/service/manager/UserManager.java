8d6a709b009ab28573c03cbaeeb475981d098f08
LABELNET <1406046087@qq.com>
2016-04-05 10:49:22 +0800

    【EDIT】业务层－用户管理－管理员登陆

--- src/main/java/cn/hpu/yuan/ssm/service/manager/UserManager.java
+++ src/main/java/cn/hpu/yuan/ssm/service/manager/UserManager.java
@@ -1,8 +1,14 @@
 package cn.hpu.yuan.ssm.service.manager;
 
+
 /**
  * Created by yuan on 16-4-4.
  * 业务层　－　后台管理－　用户管理
  */
 public interface UserManager {
+
+    //管理员登陆
+    Integer managerLogin(String unum,String pass) throws Exception;
+
+
 }
--- src/main/java/cn/hpu/yuan/ssm/service/manager/impl/UserManagerImpl.java
+++ src/main/java/cn/hpu/yuan/ssm/service/manager/impl/UserManagerImpl.java
@@ -1,10 +1,43 @@
 package cn.hpu.yuan.ssm.service.manager.impl;
 
+import cn.hpu.yuan.ssm.common.LoginStatus;
+import cn.hpu.yuan.ssm.mapper.manager.UserManagerMapper;
+import cn.hpu.yuan.ssm.model.vo.UserVo;
 import cn.hpu.yuan.ssm.service.manager.UserManager;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by yuan on 16-4-4.
  * 业务层　－　后台管理 － 用户管理 - 接口实现
  */
 public class UserManagerImpl implements UserManager{
+
+
+    @Autowired
+    private UserManagerMapper userManagerMapper;
+
+
+    /**
+     * 管理员登陆
+     * @param unum　
+     * @param pass
+     * @return
+     * @throws Exception
+     */
+    public Integer managerLogin(String unum, String pass) throws Exception {
+
+        Integer userId = userManagerMapper.findUserByUnum(unum);
+        if(userId==null){
+            return LoginStatus.UNUM_NOT_HAVE;
+        }
+        UserVo userVo = userManagerMapper.findUserByUnumPass(unum, pass);
+        if (userVo==null){
+            return LoginStatus.PASS_ERROR;
+        }
+        if(userId>0 && userVo.getId()==userId){
+            return userId;
+        }
+        return LoginStatus.Login_ERROR;
+    }
+
 }
