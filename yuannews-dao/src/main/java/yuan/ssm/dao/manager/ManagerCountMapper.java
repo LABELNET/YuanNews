ec62f0ddbca329ab0a3195f06a0c34799b4bffdd
LABELNET <1406046087@qq.com>
2016-04-07 18:29:52 +0800

    【EDIT】dao - 统计mapper接口实现

--- yuannews-dao/src/main/java/yuan/ssm/dao/manager/ManagerCountMapper.java
+++ yuannews-dao/src/main/java/yuan/ssm/dao/manager/ManagerCountMapper.java
@@ -1,5 +1,7 @@
 package yuan.ssm.dao.manager;
 
+import yuan.ssm.other.PageJo;
+
 /**
  * Created by yuan on 16-4-7.
  * 数据层－后台管理－统计相关的mapper
@@ -8,7 +10,20 @@ public interface ManagerCountMapper {
 
     /**
      * 分页信息：　
-     *
+     *　用户，新闻，评论，点赞，兴趣　：
+     * 目前：　查询总数即可，后续需要拓展的话，编写PageJo类和mapper.xml
      */
+    PageJo findUserCount() throws Exception;
 
+    PageJo findNewsCount() throws Exception;
+
+    PageJo findCommentCount() throws Exception;
+
+    PageJo findLikedCount() throws Exception;
+
+    PageJo findTasteCount() throws Exception;
+
+    /**
+     * ......
+     */
 }
