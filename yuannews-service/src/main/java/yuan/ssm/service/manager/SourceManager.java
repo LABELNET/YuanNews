66587ee7493de89e209bf228e0260b7098e9225e
LABELNET <1406046087@qq.com>
2016-04-15 23:19:52 +0800

    【EDIT】来源业务类添加，暂时未实现

--- yuannews-service/src/main/java/yuan/ssm/service/manager/SourceManager.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/SourceManager.java
@@ -1,8 +1,14 @@
 package yuan.ssm.service.manager;
 
+import yuan.ssm.service.base.ManagerBase;
+import yuan.ssm.vo.SourceVo;
+
 /**
  * Created by yuan on 16-4-4.
  * 业务层－后台管理－ 新闻来源接口
  */
-public interface SourceManager {
+public interface SourceManager extends ManagerBase<SourceVo>{
+
+    //可拓展其他接口方法
+
 }
--- yuannews-service/src/main/java/yuan/ssm/service/manager/impl/SourceManagerImpl.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/impl/SourceManagerImpl.java
@@ -1,10 +1,40 @@
 package yuan.ssm.service.manager.impl;
 
+import yuan.ssm.other.PageJo;
 import yuan.ssm.service.manager.SourceManager;
+import yuan.ssm.vo.SourceVo;
+
+import java.util.List;
 
 /**
  * Created by yuan on 16-4-4.
  * 业务层　－　后台管理 － 来源管理 - 接口实现
  */
 public class SourceManagerImpl implements SourceManager{
+
+    public List<SourceVo> managerFindList(Integer start, Integer num) throws Exception {
+        return null;
+    }
+
+    public SourceVo managerFindOne(Integer id) throws Exception {
+        return null;
+    }
+
+    public Integer managerInsertOne(SourceVo sourceVo) throws Exception {
+        return null;
+    }
+
+    public Integer managerUpdateOne(SourceVo sourceVo) throws Exception {
+        return null;
+    }
+
+    public Integer managerDeleteOne(Integer id) throws Exception {
+        return null;
+    }
+
+    public PageJo managerFindCount() throws Exception {
+        return null;
+    }
+
+
 }
