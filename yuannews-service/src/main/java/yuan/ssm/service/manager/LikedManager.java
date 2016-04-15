1b8f7499317703ac4d57906cd865f7c6027a0b76
LABELNET <1406046087@qq.com>
2016-04-15 23:23:02 +0800

    【EDIT】点赞业务方法添加

--- yuannews-service/src/main/java/yuan/ssm/service/manager/LikedManager.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/LikedManager.java
@@ -1,8 +1,16 @@
 package yuan.ssm.service.manager;
 
+import yuan.ssm.pojo.LikedPo;
+import yuan.ssm.service.base.ManagerBase;
+
 /**
  * Created by yuan on 16-4-4.
  * 业务层－后台管理－点赞管理添加
  */
-public interface LikedManager {
+public interface LikedManager extends ManagerBase<LikedPo>{
+
+    /**
+     * 其他业务方法在此拓展实现
+     */
+
 }
--- yuannews-service/src/main/java/yuan/ssm/service/manager/impl/LikedManagerImpl.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/impl/LikedManagerImpl.java
@@ -1,10 +1,37 @@
 package yuan.ssm.service.manager.impl;
 
+import yuan.ssm.other.PageJo;
+import yuan.ssm.pojo.LikedPo;
 import yuan.ssm.service.manager.LikedManager;
+
+import java.util.List;
 
 /**
  * Created by yuan on 16-4-4.
  * 业务层　－　后台管理 － 点赞管理 - 接口实现
  */
 public class LikedManagerImpl implements LikedManager {
+    public List<LikedPo> managerFindList(Integer start, Integer num) throws Exception {
+        return null;
+    }
+
+    public LikedPo managerFindOne(Integer id) throws Exception {
+        return null;
+    }
+
+    public Integer managerInsertOne(LikedPo likedPo) throws Exception {
+        return null;
+    }
+
+    public Integer managerUpdateOne(LikedPo likedPo) throws Exception {
+        return null;
+    }
+
+    public Integer managerDeleteOne(Integer id) throws Exception {
+        return null;
+    }
+
+    public PageJo managerFindCount() throws Exception {
+        return null;
+    }
 }
