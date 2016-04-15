636fd96bbc89b3dd52e091bc02d70aede9b08d78
LABELNET <1406046087@qq.com>
2016-04-15 23:24:01 +0800

    【EDIT】留言业务方法添加

--- yuannews-service/src/main/java/yuan/ssm/service/manager/CommentManager.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/CommentManager.java
@@ -1,8 +1,16 @@
 package yuan.ssm.service.manager;
 
+import yuan.ssm.pojo.CommentPo;
+import yuan.ssm.service.base.ManagerBase;
+
 /**
  * Created by yuan on 16-4-4.
  * 业务层－后台管理－评论管理接口添加
  */
-public interface CommentManager {
+public interface CommentManager extends ManagerBase<CommentPo>{
+
+    /**
+     * 其他业务方法在此拓展实现
+     */
+
 }
