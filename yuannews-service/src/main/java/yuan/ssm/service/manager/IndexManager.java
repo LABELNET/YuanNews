fdd796f8da010a7f1701643447d6d2a8f5c3ebd3
LABELNET <1406046087@qq.com>
2016-04-15 23:29:44 +0800

    【EDIT】后台主页要显示的内容，业务层添加注释

--- yuannews-service/src/main/java/yuan/ssm/service/manager/IndexManager.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/IndexManager.java
@@ -3,6 +3,16 @@ package yuan.ssm.service.manager;
 /**
  * Created by yuan on 16-4-4.
  * 业务层　－　后台管理－主页业务层
+ * 管理主页显示：　
+ * １.用户总数，新闻总数，留言总数，兴趣总数统计
+ * 2.用户最多的　兴趣　前１0个
+ * 3.新闻阅读数最多的　前１0个
+ * 4.and so on
  */
 public interface IndexManager {
+
+    /**
+     *
+     */
+
 }
