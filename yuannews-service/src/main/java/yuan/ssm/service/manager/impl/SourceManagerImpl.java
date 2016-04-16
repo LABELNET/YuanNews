fe27b1ff06126c7cff0d8c90271fc453d3c6fe7b
LABELNET <1406046087@qq.com>
2016-04-16 11:55:39 +0800

    【EDIT】来源信息业务层实现

--- yuannews-service/src/main/java/yuan/ssm/service/manager/impl/SourceManagerImpl.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/impl/SourceManagerImpl.java
@@ -1,6 +1,7 @@
 package yuan.ssm.service.manager.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import yuan.ssm.common.status.UpdateStatus;
 import yuan.ssm.dao.manager.SourceManagerMapper;
 import yuan.ssm.other.PageJo;
 import yuan.ssm.service.manager.SourceManager;
@@ -40,21 +41,53 @@ public class SourceManagerImpl implements SourceManager{
         if(id<0){
             id=1;
         }
-        return sourceManagerMapper.;
+        return sourceManagerMapper.findSourceOne(id);
     }
 
+    /**
+     * 新添加一个来源信息
+     * @param sourceVo　来源信息模型
+     * @return
+     * @throws Exception
+     */
     public Integer managerInsertOne(SourceVo sourceVo) throws Exception {
-        return null;
+        if(sourceVo==null){
+            return UpdateStatus.UPDATE_FAIL;
+        }
+        return sourceManagerMapper.insertSource(sourceVo.getSource());
     }
 
+    /**
+     * 修改信息
+     * @param sourceVo　来源信息模型
+     * @return
+     * @throws Exception
+     */
     public Integer managerUpdateOne(SourceVo sourceVo) throws Exception {
-        return null;
+        if(sourceVo==null){
+            return UpdateStatus.UPDATE_FAIL;
+        }
+        return sourceManagerMapper.updateSourceContent(sourceVo.getId(),sourceVo.getSource());
     }
 
+    /**
+     * 删除信息
+     * @param id　删除的信息id
+     * @return
+     * @throws Exception
+     */
     public Integer managerDeleteOne(Integer id) throws Exception {
-        return null;
+        if(id<0){
+            return id;
+        }
+        return sourceManagerMapper.deleteSourceContent(id);
     }
 
+    /**
+     * 查询来源信息总数（这里不实现，不需要分页）
+     * @return
+     * @throws Exception
+     */
     public PageJo managerFindCount() throws Exception {
         return null;
     }
