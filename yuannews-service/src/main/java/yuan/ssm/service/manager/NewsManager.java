9a2a9641f84c9e354b6bc32cb888014c128d90b7
LABELNET <1406046087@qq.com>
2016-04-16 14:58:39 +0800

    【EDIT】新闻业务接口实现

--- yuannews-service/src/main/java/yuan/ssm/service/manager/NewsManager.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/NewsManager.java
@@ -2,6 +2,7 @@ package yuan.ssm.service.manager;
 
 import yuan.ssm.pojo.NewsPo;
 import yuan.ssm.service.base.ManagerBase;
+import yuan.ssm.vo.NewsVo;
 
 /**
  * Created by yuan on 16-4-4.
@@ -12,5 +13,8 @@ public interface NewsManager extends ManagerBase<NewsPo>{
     /**
      * 其他业务方法在此拓展实现
      */
+
+    //添加新闻信息
+    Integer managerInsertNews(NewsVo newsVo) throws Exception;
 
 }
--- yuannews-service/src/main/java/yuan/ssm/service/manager/impl/NewsManagerImpl.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/impl/NewsManagerImpl.java
@@ -1,8 +1,14 @@
 package yuan.ssm.service.manager.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import yuan.ssm.common.status.ManagerConutStatus;
+import yuan.ssm.common.status.UpdateStatus;
+import yuan.ssm.dao.manager.ManagerCountMapper;
+import yuan.ssm.dao.manager.NewsManagerMapper;
 import yuan.ssm.other.PageJo;
 import yuan.ssm.pojo.NewsPo;
 import yuan.ssm.service.manager.NewsManager;
+import yuan.ssm.vo.NewsVo;
 
 import java.util.List;
 
@@ -12,27 +18,110 @@ import java.util.List;
  */
 public class NewsManagerImpl implements NewsManager{
 
+
+    @Autowired
+    private NewsManagerMapper newsManagerMapper;
+
+    @Autowired
+    private ManagerCountMapper managerCountMapper;
+
+    /**
+     * 新闻分页查询
+     * @param start　开始位置
+     * @param num　数量
+     * @return
+     * @throws Exception
+     */
     public List<NewsPo> managerFindList(Integer start, Integer num) throws Exception {
-        return null;
+        return newsManagerMapper.findNews(start,num);
     }
 
+    /**
+     * 查询单条信息
+     * @param id　查询的id
+     * @return
+     * @throws Exception
+     */
     public NewsPo managerFindOne(Integer id) throws Exception {
-        return null;
+        if(id<0){
+           id=1;
+        }
+        return newsManagerMapper.findNewsById(id);
     }
 
+    /**
+     * 新增加一条 - 业务类型不符合
+     * @deprecated
+     * @param newsPo
+     * @return
+     * @throws Exception
+     * @see #managerInsertNews(NewsVo)
+     */
     public Integer managerInsertOne(NewsPo newsPo) throws Exception {
-        return null;
+        return newsManagerMapper.insertNews(newsPo);
     }
 
+    /**
+     * 修改新闻信息
+     * @param newsPo
+     * @return
+     * @throws Exception
+     */
     public Integer managerUpdateOne(NewsPo newsPo) throws Exception {
-        return null;
+        int status= UpdateStatus.UPDATE_FAIL;
+
+
+        Integer id=newsPo.getId();
+        if(id<=0){
+            return status;
+        }
+
+        if(newsPo.getTitle()!=null){
+            status=newsManagerMapper.updateNewsTitle(id,newsPo.getTitle());
+        }
+
+        if(newsPo.getSid()!=null || newsPo.getSid()!=0){
+            status=newsManagerMapper.updateNewsSource(id,newsPo.getSid());
+        }
+
+        if (newsPo.getCid()!=null || newsPo.getCid()!=0){
+            status=newsManagerMapper.updateNewsCate(id,newsPo.getCid());
+        }
+
+        if(newsPo.getDt()!=null){
+            status=newsManagerMapper.updateNewsTime(id,newsPo.getDt());
+        }
+
+        return status;
     }
 
+    /**
+     * 删除新闻信息
+     * @param id　删除的信息id
+     * @return
+     * @throws Exception
+     */
     public Integer managerDeleteOne(Integer id) throws Exception {
-        return null;
+        return newsManagerMapper.deleteNewsById(id);
     }
 
+    /**
+     * 分页，查询新闻总数
+     * @return
+     * @throws Exception
+     */
     public PageJo managerFindCount() throws Exception {
-        return null;
+        return managerCountMapper.findCount(ManagerConutStatus.MANAGER_NEWS_COUNT_TYPE);
     }
+
+    /**
+     * 新添加新闻
+     * @param newsVo
+     * @return
+     * @throws Exception
+     */
+    public Integer managerInsertNews(NewsVo newsVo) throws Exception {
+        return newsManagerMapper.insertNews(newsVo);
+    }
+
 }
