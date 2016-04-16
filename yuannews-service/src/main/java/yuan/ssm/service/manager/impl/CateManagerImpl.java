9d11eeb38fbd7fe1b545a399bb783972212c32d7
LABELNET <1406046087@qq.com>
2016-04-16 16:51:04 +0800

    【EDIT】分类业务实现

--- yuannews-service/src/main/java/yuan/ssm/service/manager/impl/CateManagerImpl.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/impl/CateManagerImpl.java
@@ -1,5 +1,9 @@
 package yuan.ssm.service.manager.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import yuan.ssm.common.status.UpdateStatus;
+import yuan.ssm.dao.manager.CateManagerMapper;
+import yuan.ssm.dao.manager.ManagerCountMapper;
 import yuan.ssm.other.PageJo;
 import yuan.ssm.service.manager.CateManager;
 import yuan.ssm.vo.CateVo;
@@ -11,26 +15,81 @@ import java.util.List;
  * 业务层　－　后台管理 － 分类管理 - 接口实现
  */
 public class CateManagerImpl implements CateManager {
+
+
+    @Autowired
+    private CateManagerMapper cateManagerMapper;
+
+    @Autowired
+    private ManagerCountMapper managerCountMapper;
+
+    /**
+     * 分类查询 - 全部分类信息，没有分页
+     * @param start　开始位置
+     * @param num　数量
+     * @return
+     * @throws Exception
+     */
     public List<CateVo> managerFindList(Integer start, Integer num) throws Exception {
-        return null;
+        return cateManagerMapper.findCates();
     }
 
+    /**
+     * 查询单条分类信息
+     * @param id　查询的id
+     * @return
+     * @throws Exception
+     */
     public CateVo managerFindOne(Integer id) throws Exception {
-        return null;
+        return cateManagerMapper.findCateById(id);
     }
 
+    /**
+     *
+     * @param cateVo
+     * @return
+     * @throws Exception
+     */
     public Integer managerInsertOne(CateVo cateVo) throws Exception {
-        return null;
+        if(cateVo.getContent()==null){
+            return UpdateStatus.UPDATE_FAIL;
+        }
+        return cateManagerMapper.insertCate(cateVo.getContent());
     }
 
+    /**
+     * 更新一个
+     * @param cateVo　分类实体
+     * @return
+     * @throws Exception
+     */
     public Integer managerUpdateOne(CateVo cateVo) throws Exception {
-        return null;
+        if (cateVo.getId()<=0){
+            return UpdateStatus.UPDATE_ID_ZORE;
+        }
+
+        if(cateVo.getContent()==null){
+            return UpdateStatus.UPDATE_FAIL;
+        }
+        return cateManagerMapper.updateCate(cateVo.getId(),cateVo.getContent());
     }
 
+    /**
+     * 分类删除实现
+     * @param id　删除的信息id
+     * @return
+     * @throws Exception
+     */
     public Integer managerDeleteOne(Integer id) throws Exception {
-        return null;
+        return cateManagerMapper.deleteCate(id);
     }
 
+    /**
+     *　查询总数
+     * @return
+     * @throws Exception
+     * @deprecated 业务不需要，没必要实现
+     */
     public PageJo managerFindCount() throws Exception {
         return null;
     }
