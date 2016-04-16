3a2f80c0a61ad78fe7584f09c6a129ae714d70db
LABELNET <1406046087@qq.com>
2016-04-16 16:39:59 +0800

    【EDIT】点赞管理业务需求实现

--- yuannews-service/src/main/java/yuan/ssm/service/manager/impl/LikedManagerImpl.java
+++ yuannews-service/src/main/java/yuan/ssm/service/manager/impl/LikedManagerImpl.java
@@ -1,5 +1,9 @@
 package yuan.ssm.service.manager.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import yuan.ssm.common.status.ManagerConutStatus;
+import yuan.ssm.dao.manager.LikedManagerMapper;
+import yuan.ssm.dao.manager.ManagerCountMapper;
 import yuan.ssm.other.PageJo;
 import yuan.ssm.pojo.LikedPo;
 import yuan.ssm.service.manager.LikedManager;
@@ -11,10 +15,51 @@ import java.util.List;
  * 业务层　－　后台管理 － 点赞管理 - 接口实现
  */
 public class LikedManagerImpl implements LikedManager {
+
+    @Autowired
+    private LikedManagerMapper likedManagerMapper;
+
+    @Autowired
+    private ManagerCountMapper managerCountMapper;
+
+
+    /**
+     * 点赞，分页查询
+     * @param start　开始位置
+     * @param num　数量
+     * @return
+     * @throws Exception
+     */
     public List<LikedPo> managerFindList(Integer start, Integer num) throws Exception {
-        return null;
+        return likedManagerMapper.findLikeds(start,num);
     }
 
+
+    /**
+     * 删除某个点赞信息
+     * @param id　删除的信息id
+     * @return
+     * @throws Exception
+     */
+    public Integer managerDeleteOne(Integer id) throws Exception {
+        return likedManagerMapper.deleteLikeds(id);
+    }
+
+    /**
+     * 点赞总数
+     * @return
+     * @throws Exception
+     */
+    public PageJo managerFindCount() throws Exception {
+        return managerCountMapper.findCount(ManagerConutStatus.MANAGER_LIKED_COUNT_TYPE);
+    }
+
+    /**
+     * 下面的方法没有使用，业务需求不需要
+     * @param id　查询的id
+     * @return
+     * @throws Exception
+     */
     public LikedPo managerFindOne(Integer id) throws Exception {
         return null;
     }
@@ -27,11 +72,4 @@ public class LikedManagerImpl implements LikedManager {
         return null;
     }
 
-    public Integer managerDeleteOne(Integer id) throws Exception {
-        return null;
-    }
-
-    public PageJo managerFindCount() throws Exception {
-        return null;
-    }
 }
