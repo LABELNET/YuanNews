0ce0fa841e7603bfd4456b94f2395c9e6b0a5855
LABELNET <1406046087@qq.com>
2016-04-17 11:35:18 +0800

    【EDIT】添加后台管理主页面控制器

---
+++ yuannews-controller/src/main/java/yuan/ssm/controller/ManagerController.java
@@ -0,0 +1,35 @@
+package yuan.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ==================================================
+ * <p/>
+ * 版权：　软件工程.net12-1 原明卓
+ * <p/>
+ * 项目：　基于用户兴趣标签的新闻推荐系统
+ * <p/>
+ * 作者：　原明卓
+ * <p/>
+ * 版本：　1.0
+ * <p/>
+ * 创建日期：　16-4-17 上午11:32
+ * <p/>
+ * 功能描述： 后台管理主页控制器
+ * 功能：实现统计查询等业务
+ * 注意：管理主页面在UserController中，因为和登陆相关，所以就不迁移了！
+ * <p>
+ * <p/>
+ * 功能更新历史：
+ * <p>
+ * ==================================================
+ */
+@Controller
+@RequestMapping("/manager")
+public class ManagerController {
+
+
+
+
+}
