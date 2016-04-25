db953bf74ba3cf05c1b0d914f6f370c2156b8734
LABELNET <1406046087@qq.com>
2016-04-25 23:54:31 +0800

    【EDIT】mapper - 来源信息mapper接口信息实现

--- yuannews-dao/src/main/java/yuan/ssm/dao/customer/SourceNewsMapper.java
+++ yuannews-dao/src/main/java/yuan/ssm/dao/customer/SourceNewsMapper.java
@@ -1,5 +1,10 @@
 package yuan.ssm.dao.customer;
 
+import org.apache.ibatis.annotations.Param;
+import yuan.ssm.pojo.NewsCustom;
+
+import java.util.List;
+
 /**
  * ==================================================
  * <p/>
@@ -25,4 +30,50 @@ package yuan.ssm.dao.customer;
  * ==================================================
  */
 public interface SourceNewsMapper {
+
+    /**
+     * 根据来源，查询新闻信息,id倒序输出
+     * @param start 开始
+     * @param num 数量
+     * @param source 来源
+     * @return 新闻信息集合
+     * @throws Exception
+     */
+    List<NewsCustom> findNewsBySourceId(@Param("start") Integer start, @Param("num") Integer num, @Param("source") String source) throws Exception;
+
+
+    /**
+     * 根据来源，查询新闻信息,点赞倒序输出
+     * @param start 开始
+     * @param num 数量
+     * @param source 来源
+     * @return 新闻信息集合
+     * @throws Exception
+     */
+    List<NewsCustom> findNewsBySourceZan(@Param("start") Integer start, @Param("num") Integer num, @Param("source") String source) throws Exception;
+
+    /**
+     * 根据来源，查询新闻信息,评论倒序输出
+     * @param start 开始
+     * @param num 数量
+     * @param source 来源
+     * @return 新闻信息集合
+     * @throws Exception
+     */
+    List<NewsCustom> findNewsBySourceComment(@Param("start") Integer start, @Param("num") Integer num, @Param("source") String source) throws Exception;
+
+
+    /**
+     * 根据来源，查询新闻信息,阅读量倒序输出
+     * @param start 开始
+     * @param num 数量
+     * @param source 来源
+     * @return 新闻信息集合
+     * @throws Exception
+     */
+    List<NewsCustom> findNewsBySourceRnum(@Param("start") Integer start, @Param("num") Integer num, @Param("source") String source) throws Exception;
+
+
+
+
 }
