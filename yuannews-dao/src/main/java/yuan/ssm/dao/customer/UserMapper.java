56b343c052526e0a507078fd121cb8c1d4274df0
LABELNET <1406046087@qq.com>
2016-04-26 21:39:16 +0800

    【EDIT】mapper-用户策略之点赞，评论接口实现

--- yuannews-dao/src/main/java/yuan/ssm/dao/customer/UserMapper.java
+++ yuannews-dao/src/main/java/yuan/ssm/dao/customer/UserMapper.java
@@ -68,7 +68,7 @@ public interface UserMapper {
      * 1.添加兴趣标签,每个人最多可以添加30个兴趣标签，所以客户端需要做判断
      * 2.读取所有的兴趣标签
      * 3.查询当前用户的兴趣标签个数
-     * 4.根据用户的兴趣标签，给用户推荐的兴趣标签，插入到新闻列表中（后期）
+     * TODO 4.根据用户的兴趣标签，给用户推荐的兴趣标签，插入到新闻列表中（后期）
      * 5.分页查询兴趣标签，需要用户浏览选择；
      */
 
@@ -115,17 +115,47 @@ public interface UserMapper {
      * @param num 总数
      * @return 当前页面的兴趣标签内容
      */
-    List<TasteVo> selectTaste(@Param("state") Integer state , @Param("num") Integer num);
+    List<TasteVo> selectTaste(@Param("state") Integer state , @Param("num") Integer num) throws Exception;
 
 
     //---------------------------------------- 点赞操作--------------------------------------------
 
     /**
      * 点赞策略：
+     * 1.用户点赞修改状态，第一次为点赞，第二次为超赞;客户端进行判断type是什么点赞
+     * 2.根据id,查询当前点赞状态
+     * 3.TODO 根据用户id ,可以查询到用户的浏览记录 新闻id
      */
+
+    /**
+     * 点赞-添加点赞信息
+     * @param uid 用户id
+     * @param nid 新闻id
+     * @return 操作行
+     * @throws Exception
+     */
+    Integer userNewsZan(@Param("uid") Integer uid,@Param("nid") Integer nid) throws Exception;
+
+
+    /**
+     * 第二次点赞 - 直接根据
+     * @param uid 用户id
+     * @param nid 新闻id
+     * @return 影响行
+     * @throws Exception
+     */
+    Integer userNewsSuperZan(@Param("uid") Integer uid,@Param("nid") Integer nid) throws Exception;
+
+
 
 
     //---------------------------------------- 评论操作--------------------------------------------
 
+    /**
+     * 用户评论策略：
+     * 1.用户评论 , 评论字数限制200;
+     * 2.TODO 根据用户id 可以查询到用户评论的新闻信息
+     */
+    Integer userNewsComment(@Param("uid") Integer uid,@Param("nid") Integer nid,@Param("content") String content) throws Exception;
 
 }
