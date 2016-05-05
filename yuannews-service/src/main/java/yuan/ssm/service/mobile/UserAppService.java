4583b980370afc2feed0f26ac07405449b1c24f8
LABELNET <1406046087@qq.com>
2016-05-05 17:38:17 +0800

    【EDIT】service-实现修改密码和修改头像接口

--- yuannews-service/src/main/java/yuan/ssm/service/mobile/UserAppService.java
+++ yuannews-service/src/main/java/yuan/ssm/service/mobile/UserAppService.java
@@ -5,4 +5,11 @@ package yuan.ssm.service.mobile;
  * 服务层　－　移动端　－　用户接口
  */
 public interface UserAppService {
+
+    //修改密码
+    Integer updateUserPass(Integer id,String newPass) throws Exception;
+
+    //修改头像
+    Integer updateUserHead(Integer id,String newHead) throws Exception;
+
 }
--- yuannews-service/src/main/java/yuan/ssm/service/mobile/impl/UserAppServiceImpl.java
+++ yuannews-service/src/main/java/yuan/ssm/service/mobile/impl/UserAppServiceImpl.java
@@ -1,10 +1,43 @@
 package yuan.ssm.service.mobile.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import yuan.ssm.common.util.DateUtil;
+import yuan.ssm.common.util.FontImageUtil;
+import yuan.ssm.common.util.StringUtil;
+import yuan.ssm.dao.manager.UserManagerMapper;
 import yuan.ssm.service.mobile.UserAppService;
+
+import java.util.UUID;
 
 /**
  * Created by yuan on 16-4-4.
  * 服务层　－　移动端　－　用户接口 - 接口实现
  */
 public class UserAppServiceImpl implements UserAppService{
+
+
+    @Autowired
+    private UserManagerMapper userManagerMapper;
+
+    /**
+     * 修改用户密码
+     * @param id
+     * @param newPass
+     * @return
+     * @throws Exception
+     */
+    public Integer updateUserPass(Integer id, String newPass) throws Exception {
+        return userManagerMapper.updateUserPass(id,newPass);
+    }
+
+    /**
+     * 修改用户头像
+     * @param id
+     * @param newHead
+     * @return
+     * @throws Exception
+     */
+    public Integer updateUserHead(Integer id, String newHead) throws Exception {
+        return userManagerMapper.updateUserHead(id,newHead);
+    }
 }
