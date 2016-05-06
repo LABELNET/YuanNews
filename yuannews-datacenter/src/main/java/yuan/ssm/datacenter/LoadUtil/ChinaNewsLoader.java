b61ab38d539838619ec41e92d285c0035ce08e04
LABELNET <1406046087@qq.com>
2016-05-06 10:42:48 +0800

    【EDIT】datacenter ：添加中国新闻网主页加载类

---
+++ yuannews-datacenter/src/main/java/yuan/ssm/datacenter/LoadUtil/ChinaNewsLoader.java
@@ -0,0 +1,43 @@
+package yuan.ssm.datacenter.LoadUtil;
+
+import yuan.ssm.datacenter.base.LoaderBase;
+import yuan.ssm.datacenter.base.SourceEnum;
+
+import java.util.Set;
+
+/**
+ * ==================================================
+ * <p/>
+ * 版权：　软件工程.net12-1 原明卓
+ * <p/>
+ * 项目：　基于用户兴趣标签的新闻推荐系统
+ * <p/>
+ * 作者：　原明卓
+ * <p/>
+ * 版本：　1.0
+ * <p/>
+ * 创建日期：　16-5-6 上午10:40
+ * <p/>
+ * 功能描述： ChinaNews 中国新闻网，主页加载类
+ * （1）数据类型
+ * （2）返回url
+ * <p>
+ * <p/>
+ * 功能更新历史：
+ * <p>
+ * ==================================================
+ */
+public class ChinaNewsLoader extends LoaderBase{
+
+    public ChinaNewsLoader(String url) {
+        super(url);
+    }
+
+    protected Set<String> getUrls() {
+        return null;
+    }
+
+    protected SourceEnum getType() {
+        return SourceEnum.chinanews;
+    }
+}
